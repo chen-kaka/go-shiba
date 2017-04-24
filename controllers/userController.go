0020655ddbc95445d5f4004664544c036d00cc0b
kakachan <chen-kaka@163.com>
2017-04-24 10:51:52 +0800

    error codes

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"go-shiba/services"
+	"go-shiba/middlewares/exception"
 )
 
 type(
@@ -16,7 +17,7 @@ http://localhost:9000/user?id=58fc5435ce01cec8e7f6ec40
 func (c *UserController) Get() {
 	userId := c.GetString("id")
 	if userId == "" {
-		c.retError(&c.BaseController, 1, "param error.")
+		c.retError(&c.BaseController, exception.PARAM_MISSING, "id param required.")
 		return
 	}
 	
---
+++ middlewares/exception/errorCode.go
@@ -0,0 +1,26 @@
+package exception
+
+const (
+	// All kinds of error code definitions
+	
+	// client error code
+	PARAM_ERROR = 10001
+	PARAM_MISSING = 10002
+	PARAM_NOT_ALLOWED = 10003
+	PASSWORD_ERROR = 10004
+	LOGIN_FAILED = 10005
+	
+	// server error code
+	SERVICE_ERROR = 20001
+	DATABASE_ERROR = 20002
+	MIDDLEWARE_ERROR = 20003
+	DATA_ERROR = 20004
+	UNKNOWN_ERROR = 20005
+	
+	// interface error code
+	INTERFACE_ERROR = 30001
+	INTERFACE_PENDING = 30002
+	
+	//define other error code here
+	
+)
